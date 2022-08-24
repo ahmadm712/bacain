fc3f1be242c655af7375ab7bc3a7fb59ac893712
ahmadm712 <ahmadmuji2808@gmail.com>
2022-08-24 15:43:53 +0700

    add fvm config dll

---
+++ lib/styles/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+Color kColorPrimary = const Color(0xffBB2C1A);
---
+++ lib/styles/theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var textTheme = TextTheme(
+  headline1: GoogleFonts.ibmPlexSansCondensed(
+      fontSize: 99, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+  headline2: GoogleFonts.ibmPlexSansCondensed(
+      fontSize: 62, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+  headline3: GoogleFonts.ibmPlexSansCondensed(
+      fontSize: 49, fontWeight: FontWeight.w400),
+  headline4: GoogleFonts.ibmPlexSansCondensed(
+      fontSize: 35, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+  headline5: GoogleFonts.ibmPlexSansCondensed(
+      fontSize: 25, fontWeight: FontWeight.w400),
+  headline6: GoogleFonts.ibmPlexSansCondensed(
+      fontSize: 21, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+  subtitle1: GoogleFonts.ibmPlexSansCondensed(
+      fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+  subtitle2: GoogleFonts.ibmPlexSansCondensed(
+      fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+  bodyText1: GoogleFonts.ibmPlexSans(
+      fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+  bodyText2: GoogleFonts.ibmPlexSans(
+      fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+  button: GoogleFonts.ibmPlexSans(
+      fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+  caption: GoogleFonts.ibmPlexSans(
+      fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+  overline: GoogleFonts.ibmPlexSans(
+      fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+);
